80876cbec684992a5d2359cf23e15226089033c4
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2018-05-23 16:46:51 +0200

    Support CentOS mirrors (#7)
    NethServer/dev#5505

--- nethserver.php
+++ nethserver.php
@@ -5,16 +5,24 @@ $nsrelease = $_GET['nsrelease'];
 $arch = $_GET['arch'];
 $repo = $_GET['repo'];
 
-$valid_release = in_array($release, array('6', '7'));
-$valid_nsrelease = in_array($nsrelease, array('6.8','6.9','7.3.1611','7.4.1708','7.5.1804'));
-$valid_arch = in_array($arch, array('x86_64'));
-$valid_repo = in_array($repo, array(
+$ns_repos = array(
     'base',
     'updates',
     'testing',
     'nethforge',
     'nethforge-testing'
-));
+);
+
+$ce_repos = array(
+    'ce-base' => 'os',
+    'ce-updates' => 'updates',
+    'ce-extras' => 'extras',
+);
+
+$valid_release = in_array($release, array('6', '7'));
+$valid_nsrelease = in_array($nsrelease, array('6.8','6.9','7.3.1611','7.4.1708','7.5.1804'));
+$valid_arch = in_array($arch, array('x86_64'));
+$valid_repo = in_array($repo, array_merge($ns_repos,array_keys($ce_repos)));
 
 # nsrelease overrides release
 if ( $valid_nsrelease ) {
@@ -30,6 +38,9 @@ header('Content-type: text/plain; charset=UTF-8');
 
 if($repo === 'testing' || $repo === 'nethforge-testing') {
     $mirrors = array('http://packages.nethserver.org/nethserver');
+} else if (in_array($repo, array_keys($ce_repos))) {
+    $mirrors = file("ce-mirrors");
+    $repo = $ce_repos[$repo]; # map to real repository name
 } else {
     $mirrors = file("mirrors");
 }
